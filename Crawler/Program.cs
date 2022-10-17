e48630d0c9eca20ae8798ed6c2e2bf454cb625e9
Lukasz Rybak <lukirybak@gmail.com>
2022-10-17 23:41:29 +0200

    Dodano obsluge bledow i zwalnianie zasobow z httpClient

--- Crawler/Program.cs
+++ Crawler/Program.cs
@@ -9,18 +9,18 @@ namespace Crawler
     {
         static ArrayList ExtractEmails(string data)
         {
- 
+
             Regex emailRegex = new Regex(@"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", RegexOptions.IgnoreCase);
             MatchCollection emailMatches = emailRegex.Matches(data);
 
-            var result = new ArrayList(); // recommended 
+            var result = new ArrayList();
             foreach (Match emailMatch in emailMatches)
             {
                 if (!result.Contains(emailMatch.Value))
                 {
                     result.Add(emailMatch.Value);
-                }; 
-                
+                };
+
             }
             return result;
         }
@@ -29,18 +29,54 @@ namespace Crawler
         public static async Task Main(string[] args)
         {
 
-            string url = args[0];
-            HttpClient httpClient = new HttpClient();
-            HttpResponseMessage response = await httpClient.GetAsync(url);
-            string responseContent = await response.Content.ReadAsStringAsync();
-            ArrayList emails = ExtractEmails(responseContent);
 
-            foreach (string e in emails)
+
+
+            try
             {
-                Console.WriteLine(e);
+                if (args.Length == 0)
+                {
+                    throw new ArgumentNullException("URL");
+                }
+                string url = args[0];
+
+                if (!Uri.IsWellFormedUriString(url, UriKind.Absolute))
+                {
+                    throw new ArgumentException("Niepoprawny URL");
+                }
+
+                HttpClient httpClient = new HttpClient();
+                HttpResponseMessage response = await httpClient.GetAsync(url);
+                httpClient.Dispose();
+                string responseContent = await response.Content.ReadAsStringAsync();
+                ArrayList emails = ExtractEmails(responseContent);
+
+                if (emails.Count > 0)
+                {
+                    foreach (string e in emails)
+                    {
+                        Console.WriteLine(e);
+
+                    }
+                }else { Console.WriteLine("Nie znaleziono żadnego adresu email"); }
+
 
             }
-            
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("Błąd w czasie pobierania strony");
+            }
+
+
+
 
         }
     }

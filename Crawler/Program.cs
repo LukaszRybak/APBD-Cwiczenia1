04010f0c9ba64b409d87e0507bfa16569ae5929e
Lukasz Rybak <lukirybak@gmail.com>
2022-10-17 22:46:33 +0200

    Bazowy program + Unikatowe wypisywanie emaili

--- Crawler/Program.cs
+++ Crawler/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections;
+using System.Linq;
+using System.Text.RegularExpressions;
+using static System.Net.WebRequestMethods;
+
+namespace Crawler
+{
+    public class Program
+    {
+        static ArrayList ExtractEmails(string data)
+        {
+ 
+            Regex emailRegex = new Regex(@"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", RegexOptions.IgnoreCase);
+            MatchCollection emailMatches = emailRegex.Matches(data);
+
+            var result = new ArrayList(); // recommended 
+            foreach (Match emailMatch in emailMatches)
+            {
+                if (!result.Contains(emailMatch.Value))
+                {
+                    result.Add(emailMatch.Value);
+                }; 
+                
+            }
+            return result;
+        }
+
+
+        public static async Task Main(string[] args)
+        {
+
+            string url = args[0];
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage response = await httpClient.GetAsync(url);
+            string responseContent = await response.Content.ReadAsStringAsync();
+            ArrayList emails = ExtractEmails(responseContent);
+
+            foreach (string e in emails)
+            {
+                Console.WriteLine(e);
+
+            }
+            
+
+        }
+    }
+}
